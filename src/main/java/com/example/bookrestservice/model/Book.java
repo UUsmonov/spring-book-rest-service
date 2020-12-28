6c71a2b4633da2560b111e0dbd8f49a698a78d5f
UUsmonov <bek.usmonov@gmail.com>
2020-12-28 08:38:16 +0500

    H2 integration

--- src/main/java/com/example/bookrestservice/model/Book.java
+++ src/main/java/com/example/bookrestservice/model/Book.java
@@ -1,15 +1,21 @@
 package com.example.bookrestservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue
     private Integer id;
     private String title;
     private String isbn;
